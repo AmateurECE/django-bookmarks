91584e673d2f36548a2ef9763acc55e128b4e385
AmateurECE <edtwardy@mtu.edu>
2020-06-21 01:10:55 -0400

    Add initial database migration

--- models.py
+++ models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Bookmark(models.Model):
+    pageLink = models.CharField(max_length=512)
+    pageTitle = models.CharField(max_length=128)
