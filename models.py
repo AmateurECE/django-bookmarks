691ad97cdfcd6176a0e16f2564566d434594c7b9
AmateurECE <edtwardy@mtu.edu>
2020-06-21 13:11:43 -0400

    Initial index template and route
    This commit creates a working route to an index page that displays
    the bookmarks in a list format, with a navbar.

--- models.py
+++ models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class Folder(models.Model):
+    name = models.CharField(max_length=16)
+
 class Bookmark(models.Model):
     pageLink = models.CharField(max_length=512)
     pageTitle = models.CharField(max_length=128)
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
--- views.py
+++ views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import Folder, Bookmark
+
 # Create your views here.
+def index(request):
+    folderList = Folder.objects.order_by('name')
+    context = {'folderList': None}
+    if folderList:
+        for folder in folderList:
+            bookmarks = Bookmark.objects.filter(folder=folder)
+            folder.bookmarks = bookmarks
+        context = {'folderList': folderList}
+    return render(request, 'bookmarks/index.html', context)
