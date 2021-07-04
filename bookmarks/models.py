86eb926b99ce9bf88d96be5a7d12cd11b1ea9542
AmateurECE <edtwardy@mtu.edu>
2021-07-04 11:42:32 -0500

    1. Fix models.W042 for Bookmark and Folder models:
    bookmarks.Bookmark: (models.W042) Auto-created primary key used
    when not defining a primary key type, by default
    'django.db.models.AutoField'.
    
    2. Update version to 1.0 in preparation for release
    
    3. Add migration for latest model change
    
    4. Add Pillow as dependency

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -3,12 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Folder(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=16)
 
     class Meta:
         ordering = ['name']
 
 class Bookmark(models.Model):
+    id = models.AutoField(primary_key=True)
     pageLink = models.CharField(max_length=512)
     pageTitle = models.CharField(max_length=128)
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='django-bookmarks',
-    version='0.1.0',
+    version='1.0',
     author='Ethan D. Twardy',
     author_email='ethan.twardy@gmail.com',
     description='Django application for creating and storing bookmarks',
@@ -17,7 +17,11 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.5',
-    install_requires=['django', 'djangorestframework'],
+    install_requires=[
+        'django',
+        'djangorestframework',
+        'Pillow',
+    ],
     provides=['django_bookmarks'],
     include_package_data=True,
 )
